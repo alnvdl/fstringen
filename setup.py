801b743cb8aa35920cd92fb30fd1253394b80b6d
Allan Vidal <allan.vidal@canonical.com>
2019-10-04 19:54:27 +0000

    Bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="fstringen",
-    version="0.0.1",
+    version="0.0.2",
     author="Allan Vidal",
     description="A text generator based on f-strings",
     url="https://github.com/alnvdl/fstringen",
