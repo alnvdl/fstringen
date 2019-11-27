cb78eec74ae27d20f88516db8e6971b3cbe2132b
Allan Vidal <allan.vidal@canonical.com>
2019-11-27 20:38:09 +0000

    Improve error reporting

--- fstringen/__init__.py
+++ fstringen/__init__.py
@@ -1,1 +1,2 @@
-from fstringen.fstringen import gen, Model, Mapper, FStringenError, FStringenNotFound
+from fstringen.fstringen import gen, Selectable, Model, Mapper, \
+    FStringenError, SelectableError
